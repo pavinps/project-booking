a6f4a3e86fc3dac0336f9b08a8b89ace4bc0d8ac
pavinps <pavin748@gmail.com>
2022-09-26 12:20:16 +0100

    delete-completed

--- src/Components/Popup.jsx
+++ src/Components/Popup.jsx
@@ -1,95 +1,99 @@
 import React, { useState } from 'react'
 import Input from './Input'
 import './popup.css'
-import Button from  './Button'
+import Button from './Button'
 import Amenities from './Amenities'
 import apiCall from '../serivce/apiCall'
 import { useEffect } from 'react'
 
-const Popup = ({setaddroom,editid,setEditId,roomData}) => {
+const Popup = ({ setaddroom, editid, setEditId, roomData }) => {
   const [formData, setFormData] = useState({
-    roomNumber:"",
-    adultCapacity:"",
-    childCapacity:"",
-    price:""
+    roomNumber: "",
+    adultCapacity: "",
+    childCapacity: "",
+    price: ""
   })
- 
-  useEffect(()=>{
-    setFormData(roomData.find(r=>r.id === editid))
-  },[editid])
-  const {roomNumber,adultCapacity,childCapacity,price }= formData;
+
+  useEffect(() => {
+    console.log(editid);
+    if(editid)setFormData(roomData.find(r => r.id === editid))
+   
+  }, [editid])
   
-  const onChange=(value,key)=>{
+  const { roomNumber, adultCapacity, childCapacity, price } = formData;
+
+  const onChange = (value, key) => {
     setFormData({
       ...formData,
-      [key]:value
+      [key]: value
     })
 
   }
- 
-   const [Selectarray, setSelectarray] = useState([])
-  const senddata=async(e)=>{
+
+  const [Selectarray, setSelectarray] = useState([])
+  const senddata = async (e) => {
     e.preventDefault()
-    console.log(formData);
+
     let res;
     if (editid) {
-      res=await updateroom()
-      
-    }else{
-      console.log("add");
-      res=addRoom()
+      res = await updateroom()
+
+    } else {
+      // console.log("add");
+      res =await addRoom()
     }
-      closeWindow();
+    closeWindow();
   };
-  const updateroom=()=>apiCall(`/rooms/${formData.id}`,"PUT",formData)
-  const addRoom=()=>apiCall("/rooms","POST",formData)
-  const closeWindow =()=>{
-    setaddroom(false);
+  const updateroom = () => apiCall(`/rooms/${formData.id}`, "PUT", formData)
+  const addRoom = () => apiCall("/rooms", "POST", formData)
+
+  const closeWindow = () => {
     setEditId(null);
-    
+    setaddroom(false);
+
   }
   return (
     <div className='popup' >
       <div className='popup-head'>
         <div className='name'> Room</div>
-        <div onClick={()=>(setaddroom(false))}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" height='20px' className='cancel'><path d="M175 175C184.4 165.7 199.6 165.7 208.1 175L255.1 222.1L303 175C312.4 165.7 327.6 165.7 336.1 175C346.3 184.4 346.3 199.6 336.1 208.1L289.9 255.1L336.1 303C346.3 312.4 346.3 327.6 336.1 336.1C327.6 346.3 312.4 346.3 303 336.1L255.1 289.9L208.1 336.1C199.6 346.3 184.4 346.3 175 336.1C165.7 327.6 165.7 312.4 175 303L222.1 255.1L175 208.1C165.7 199.6 165.7 184.4 175 175V175zM512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 48C141.1 48 48 141.1 48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48z"/></svg></div>
-        </div>
-       <form action="" onSubmit={senddata}>
+        <div onClick={closeWindow}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" height='20px' className='cancel'><path d="M175 175C184.4 165.7 199.6 165.7 208.1 175L255.1 222.1L303 175C312.4 165.7 327.6 165.7 336.1 175C346.3 184.4 346.3 199.6 336.1 208.1L289.9 255.1L336.1 303C346.3 312.4 346.3 327.6 336.1 336.1C327.6 346.3 312.4 346.3 303 336.1L255.1 289.9L208.1 336.1C199.6 346.3 184.4 346.3 175 336.1C165.7 327.6 165.7 312.4 175 303L222.1 255.1L175 208.1C165.7 199.6 165.7 184.4 175 175V175zM512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 48C141.1 48 48 141.1 48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48z" /></svg></div>
+      </div>
+      <form action="" onSubmit={senddata}>
         <div className='box-one'>
-        <Input title="Room no" type='number' setstate={v=>onChange(v,"roomNumber")} value={roomNumber}/>
-        <Input title="Adult capacity"  type='number' setstate={v=>onChange(v,"adultCapacity")} value={adultCapacity}/>
-        <Input title="Children capacity"  type='number'setstate={v=>onChange(v,"childCapacity")} value={childCapacity}/>
-        <Input title="Price"  type='number'setstate={v=>onChange(v,"price")} value={price}/>
+          <Input title="Room no" type='number' setstate={v => onChange(v, "roomNumber")} value={roomNumber} />
+          <Input title="Adult capacity" type='number' setstate={v => onChange(v, "adultCapacity")} value={adultCapacity} />
+          <Input title="Children capacity" type='number' setstate={v => onChange(v, "childCapacity")} value={childCapacity} />
+          <Input title="Price" type='number' setstate={v => onChange(v, "price")} value={price} />
         </div>
         <div className='save'>
-        <Button color='white' text='save'back='#d7ae63' padding="10px" wid='92px' hi='40px'/>
-        
+          <Button color='white' text='save' back='#d7ae63' padding="10px" wid='92px' hi='40px' />
+
         </div>
 
-        </form>
-        {editid &&
+      </form>
+      {editid &&
         <div className='amen'>
-        <div>Amenities</div>
-        <select className='select' onChange={(e)=>{setSelectarray([...Selectarray,e.target.value])}}>
-          <option>Select</option>
-          <option value="Television">Television</option>
-          <option value="Air-Conditioner">Air Conditioner</option>
-          <option value="internet-access">internet access</option>
+          <div>Amenities</div>
+          <select className='select' onChange={(e) => { setSelectarray([...Selectarray, e.target.value]) }}>
+            <option>Select</option>
+            <option value="Television">Television</option>
+            <option value="Air-Conditioner">Air Conditioner</option>
+            <option value="internet-access">internet access</option>
 
-         </select>
-         <div>
-        {Selectarray.map((data,index)=>{
-          return(
-            <Amenities select={Selectarray} data={data} key={index} index={index} Selectarray={setSelectarray} />
-            
-          )
-        })}
-      </div>
-      </div>
-}
+          </select>
+          <div>
+            {Selectarray.map((data, index) => {
+              return (
+                <Amenities select={Selectarray} data={data} key={index} index={index} Selectarray={setSelectarray} />
+
+              )
+            })}
+          </div>
         </div>
-   
-   
+      }
+    </div>
+
+
   )
 }
 
--- src/Components/Roomdata.jsx
+++ src/Components/Roomdata.jsx
@@ -4,73 +4,99 @@ import Button from './Button'
 import Popup from './Popup'
 import { useEffect } from 'react'
 import apiCall from '../serivce/apiCall'
-const Roomdata = ({btn_text}) => {
+const Roomdata = ({ btn_text }) => {
     const [roomData, setRoomData] = useState([])
     const [addroom, setaddroom] = useState(false)
     const [editid, setEditId] = useState(null)
-    useEffect(()=>{
-        apiCall("/rooms","GET")
-       
-        .then(respones=>{
-            setRoomData(respones);
-            console.log(roomData);
-        })
-    },[setaddroom])
- function popuproom() {
-    console.log(setaddroom);
-    setaddroom(true);
-    
- }
+    const [deleteid, setdeleteid] = useState(null)
+
+    useEffect(() => {
+        apiCall("/rooms", "GET")
+
+            .then(respones => {
+                setRoomData(respones);
+                console.log(roomData);
+            })
+    }, [setaddroom])
+    function popuproom() {
+        console.log(setaddroom);
+        setaddroom(true);
+
+    }
+    const deleteroom=async()=>{
+        const res=await apiCall(`/rooms/${deleteid}`,"DELETE")
+        setdeleteid(null)
+    }
 
 
-  return (
-    <div className='roomdata-main'>
-        <div className='roomdata-head'>
-            Room
-        <div className='roomdata-adbtn'>
-        <Button color='white' text={btn_text}back='#d7ae63' padding="10px" wid='92px' hi='40px' funtionality={popuproom}/>
-        </div>
-        </div>
-        <div className='roomdata-table'>
-            <div className='roomdata-heading'>
-                <div>
-                    Room no
+    return (
+        <div className='roomdata-main'>
+            <div className='roomdata-head'>
+                Room
+                <div className='roomdata-adbtn'>
+                    <Button color='white' text={btn_text} back='#d7ae63' padding="10px" wid='92px' hi='40px' funtionality={popuproom} />
                 </div>
-                <div>
-                    Adult capacity
-                </div>
-                <div>
-                    children capacity
-                </div>
-                <div>
-                    Price
-                </div>
-                
             </div>
-            <div className='roomdata-row'>
-                    {roomData?.map((data,index)=>{
-                        return(
+            <div className='roomdata-table'>
+                <div className='roomdata-heading'>
+                    <div>
+                        Room no
+                    </div>
+                    <div>
+                        Adult capacity
+                    </div>
+                    <div>
+                        children capacity
+                    </div>
+                    <div>
+                        Price
+                    </div>
+
+                </div>
+                <div className='roomdata-row'>
+                    {roomData?.map((data, index) => {
+                        return (
                             <div className='roomdata-data' key={data.id}>
-                             <div>{data.roomNumber}</div>
-                            <div>{data.adultCapacity}</div>
-                            <div>{data.childCapacity}</div>
-                            <div>{data.price}</div>
-                            <svg  onClick={()=>{
-                                setaddroom(true);
-                                setEditId(data.id);
-                 
-                            }} height="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512"><path d="M64 360c30.9 0 56 25.1 56 56s-25.1 56-56 56s-56-25.1-56-56s25.1-56 56-56zm0-160c30.9 0 56 25.1 56 56s-25.1 56-56 56s-56-25.1-56-56s25.1-56 56-56zM120 96c0 30.9-25.1 56-56 56S8 126.9 8 96S33.1 40 64 40s56 25.1 56 56z"/></svg>
+                                <div>{data.roomNumber}</div>
+                                <div>{data.adultCapacity}</div>
+                                <div>{data.childCapacity}</div>
+                                <div>{data.price}</div>
+
+                                <div><svg onClick={() => {
+                                    setaddroom(true)
+                                    setEditId(data.id);
+
+                                }} height="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512"><path d="M64 360c30.9 0 56 25.1 56 56s-25.1 56-56 56s-56-25.1-56-56s25.1-56 56-56zm0-160c30.9 0 56 25.1 56 56s-25.1 56-56 56s-56-25.1-56-56s25.1-56 56-56zM120 96c0 30.9-25.1 56-56 56S8 126.9 8 96S33.1 40 64 40s56 25.1 56 56z" /></svg>
+                                </div>
+                                <div onClick={()=>{setdeleteid(data.id)}}><svg xmlns="http://www.w3.org/2000/svg"
+                                    height='20px' viewBox="0 0 448 512"><path d="M160 400C160 408.8 152.8 416 144 416C135.2 416 128 408.8 128 400V192C128 183.2 135.2 176 144 176C152.8 176 160 183.2 160 192V400zM240 400C240 408.8 232.8 416 224 416C215.2 416 208 408.8 208 400V192C208 183.2 215.2 176 224 176C232.8 176 240 183.2 240 192V400zM320 400C320 408.8 312.8 416 304 416C295.2 416 288 408.8 288 400V192C288 183.2 295.2 176 304 176C312.8 176 320 183.2 320 192V400zM317.5 24.94L354.2 80H424C437.3 80 448 90.75 448 104C448 117.3 437.3 128 424 128H416V432C416 476.2 380.2 512 336 512H112C67.82 512 32 476.2 32 432V128H24C10.75 128 0 117.3 0 104C0 90.75 10.75 80 24 80H93.82L130.5 24.94C140.9 9.357 158.4 0 177.1 0H270.9C289.6 0 307.1 9.358 317.5 24.94H317.5zM151.5 80H296.5L277.5 51.56C276 49.34 273.5 48 270.9 48H177.1C174.5 48 171.1 49.34 170.5 51.56L151.5 80zM80 432C80 449.7 94.33 464 112 464H336C353.7 464 368 449.7 368 432V128H80V432z" /></svg>
+                                </div>
+
                             </div>
-                           
+
                         )
                     })}
                 </div>
-<div className={addroom ? "popwindow":"" }>
-    {addroom && <Popup setaddroom={setaddroom} editid={editid} setEditId={setEditId} roomData={roomData} />}
-</div>
+
+                <div className={addroom || deleteid ? "popwindow" : ""}>
+                    {addroom && <Popup setaddroom={setaddroom} editid={editid} setEditId={setEditId} roomData={roomData} />}
+                    {
+                        deleteid && 
+                        
+                        <div className="delete_popup">
+                            <div className="delete_text"><h3>are you sure</h3></div>
+                            <div className="delete_buttons">
+                                <button onClick={deleteroom}>Delete</button>
+                                <button onClick={()=>{setdeleteid(null)}}>close</button>
+                            </div>
+                        
+                        </div>
+                    }
+                </div>
+
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Roomdata
